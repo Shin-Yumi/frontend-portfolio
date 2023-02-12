d86eec9b720be6cf52ffdc36ee7318db35f2a0a6
Shin-Yumi <tlsdbal0517@naver.com>
2023-02-12 22:37:22 +0900

    emailjs 추가

--- js/contact.js
+++ js/contact.js
@@ -1,1 +1,27 @@
-//D0ABDF498CA79678AA37A2AF25A05A7EA852
+//service_a67ezzs
+const form = document.querySelector("#contactForm");
+const submit = form.querySelector(".submit");
+
+function sendEmail() {
+  emailjs.init("smXrTUT7qtrtDtUhx");
+  let templateParams = {
+    name: document.getElementById("contactName").value,
+    phone: document.getElementById("contactPhone").value,
+    email: document.getElementById("contactEmail").value,
+    message: document.getElementById("contactMsg").value,
+  };
+  console.log(templateParams);
+  emailjs.send("service_a67ezzs", "template_g0rfbw9", templateParams).then(
+    function () {
+      console.log("SUCCESS!");
+    },
+    function (error) {
+      console.log("FAILED...", error);
+    }
+  );
+}
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  sendEmail();
+});
